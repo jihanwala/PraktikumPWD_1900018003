8508b87676546b4b167157673ebeb0e516546790
Jihan Wala <62744196+jihanwala@users.noreply.github.com>
2021-10-21 19:34:45 +0700

    praktik pertemuan 3

---
+++ praktik pert 3/delete.php
@@ -0,0 +1,13 @@
+<?php
+//include database connection file
+include_once("koneksi.php");
+
+//get id from url to delete that user
+$nim = $_GET ['nim'];
+
+//Delete user row from table based on given id
+$result = mysqli_query($con, "DELETE FROM mahasiswa WHERE nim='$nim'");
+
+//after delete redirect to Home, so that lates user list will be displayed
+header("Location:index.php")
+?>
