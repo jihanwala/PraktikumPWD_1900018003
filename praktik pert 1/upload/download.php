ee5c76eefdd9048a09887dbc7a316426df02dd20
Jihan Wala <62744196+jihanwala@users.noreply.github.com>
2021-11-04 19:02:16 +0700

    Delete praktik pert 1 directory

--- praktik pert 1/upload/download.php
+++
@@ -1,9 +0,0 @@
-<?php
-$myDir = "c:/xampp/htdocs/pwd2021/praktik pert 1/upload/";
-$dir = opendir($myDir);
-echo "Isi folder (klik link untuk download : <br>";
-while($tmp = readdir($dir)){
-echo "<a href='$tmp' target='_blank'>$tmp</a><br>";
-}
-closedir($dir);
-?>
